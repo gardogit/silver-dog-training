641217b934987138d97463453f58b1a477290872
gardogit <edgardo.irm@gmail.com>
2025-08-17 01:26:02 -0400

    integracion de secciones de componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { HeroSection } from "@/components/sections/HeroSection";
+import { ServicesSection } from "@/components/sections/ServicesSection";
+import { GoogleReviewsSection } from '@/components/sections/GoogleReviewsSection';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <ServicesSection />
+      <GoogleReviewsSection />
     </main>
   );
 }
